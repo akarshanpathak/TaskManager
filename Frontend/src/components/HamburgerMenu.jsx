f8b4d1b68f0d6473baf0eae81441cbb1252ed995
akarshanpathak <akarshanpathak2411@gmail.com>
2024-08-05 13:16:51 +0530

    complete build setup

--- Frontend/src/components/HamburgerMenu.jsx
+++ Frontend/src/components/HamburgerMenu.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import {Link} from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
+import { signOutSuccess } from '../redux/userSlice.js'
 function HamburgerMenu() {
   const navigate=useNavigate()
+  
+  const dispatch=useDispatch()
+  const handleSignout=async()=>{
+    try {
+      const res=await fetch("/api/auth/signout",{
+        method:'PUT'
+      })
+      if(res.ok){
+        dispatch(signOutSuccess())
+        navigate('/signin')
+        // console.log("signout successfull");
+      }
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
   return (
     <div className='p-6 border-b-8 border-blue-300 rounded-bl-lg rounded-br-lg duration-150 lg:hidden'>
        <ul >
@@ -10,6 +29,7 @@ function HamburgerMenu() {
        <li className='mt-4 cursor-pointer hover:underline hover:text-blue-300 '>About</li>
         <Link to='/?tab=all-task'><li className='mt-4 cursor-pointer hover:underline hover:text-blue-300 '>All Tasks</li></Link>
        <Link to='/?tab=create-task'> <li className='mt-4 cursor-pointer hover:underline hover:text-blue-300 '>Create Task</li></Link>
+       <li onClick={handleSignout} className='mt-4 cursor-pointer hover:underline hover:text-red-500 '>Sign Out</li>
        </ul>
     </div>
   )
--- index.js
+++ index.js
@@ -5,9 +5,10 @@ import cors from 'cors';
 import authRouter from './routes/auth.route.js';
 import { connectDb } from './dbConnect/connect.js';
 import { createtask, deleteTask, getStats, getTask, getUserTask, updateTask } from './controllers/task.controller.js';
+import path from 'path'
 
 dotenv.config(); // dotenv configuration
-
+const __dirname = path.resolve();
 connectDb();
 const app = express();
 
@@ -26,7 +27,13 @@ app.put("/api/task/:taskId", updateTask);
 app.get("/api/user/tasks/:userId", getUserTask);
 app.get("/api/user/tasks/stats/:userId", getStats);
 
+app.use(express.static(path.join(__dirname, '/Frontend/dist')));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Frontend', 'dist', 'index.html'));
+  });
